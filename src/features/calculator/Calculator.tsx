b792965ce773f571ef933afcd62375e8ac687dc2
Merzaad <Merzaad@gmail.com>
2022-02-04 12:01:48 +0330

    Calculator component

--- src/features/calculator/Calculator.tsx
+++ src/features/calculator/Calculator.tsx
@@ -8,8 +8,34 @@ import { selectValue } from './calculatorSlice';
 function Calculator() {
   const test = useAppSelector(selectValue);
   return (
-    <div className="container">
-      {test}
+    <div className="container w-25">
+      <div className="container bg-dark text-white">
+        {test}
+      </div>
+      <div className="container d-flex justify-content-center my-1">
+        <button type="button" className="btn btn-secondary">1</button>
+        <button type="button" className="btn btn-secondary">2</button>
+        <button type="button" className="btn btn-secondary">3</button>
+        <button type="button" className="btn btn-secondary">-</button>
+      </div>
+      <div className="container d-flex justify-content-center my-1">
+        <button type="button" className="btn btn-secondary">4</button>
+        <button type="button" className="btn btn-secondary">5</button>
+        <button type="button" className="btn btn-secondary">6</button>
+        <button type="button" className="btn btn-secondary">+</button>
+      </div>
+      <div className="container d-flex justify-content-center my-1">
+        <button type="button" className="btn btn-secondary">7</button>
+        <button type="button" className="btn btn-secondary">8</button>
+        <button type="button" className="btn btn-secondary">9</button>
+        <button type="button" className="btn btn-secondary">=</button>
+      </div>
+      <div className="container d-flex justify-content-center my-1">
+        <button type="button" className="btn btn-secondary">*</button>
+        <button type="button" className="btn btn-secondary">/</button>
+        <button type="button" className="btn btn-secondary">^</button>
+        <button type="button" className="btn btn-secondary">#</button>
+      </div>
     </div>
   );
 }
