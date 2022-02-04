15b3bf9a48244cf182b02308cdffc207fa33ec34
Merzaad <Merzaad@gmail.com>
2022-02-04 17:14:09 +0330

    Router

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import {
+  BrowserRouter, Routes, Route, Outlet, Link,
+} from 'react-router-dom';
+
 import App from './App';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <Link to="/">home</Link>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />}>
+            <Route index element={<App />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
